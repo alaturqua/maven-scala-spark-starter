9ef375d19ddacbe218fcff351baf5aa5c8cfaee2
Isa Inalcik <dynamit1985@gmail.com>
2021-10-24 15:13:41 +0200

    add examples

--- src/main/scala/com/example/ScalaSparkExample.scala
+++ src/main/scala/com/example/ScalaSparkExample.scala
@@ -1,44 +1,62 @@
 package com.example
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types._
 
 /**
  * @author ${user.name}
  */
+
+case class Person(name: String, age: Int)
+
 object ScalaSparkExample {
 
-  def main(args : Array[String]): Unit = {
-    val spark = getSparkSession
-    import spark.implicits._
-    Logger.getLogger("org").setLevel(Level.FATAL)
+  def main(args: Array[String]): Unit = {
+
 
     try {
-      val df = spark.read.json("file:/C:/tools/spark/examples/src/main/resources/people.json")
-      df.printSchema()
-      df.select("name").show()
-      df.select($"name", $"age" + 1).show()
-      df.filter($"age" > 21).show()
-      df.groupBy("age").count().show()
+      val spark = getSparkSession
+      import spark.implicits._
+      Logger.getLogger("org").setLevel(Level.FATAL)
 
-      df.createOrReplaceTempView("people")
+      val data = Seq(
+        Row(8, "bat"),
+        Row(64, "mouse"),
+        Row(-27, "horse"),
+        Row(-27, "horse"),
+        Row(-27, "horse")
+      )
+      val schema = StructType(
+        List(
+          StructField("number", IntegerType, nullable = true),
+          StructField("word", StringType, nullable = true)
+        )
+      )
+      val df = spark.createDataFrame(
+        spark.sparkContext.parallelize(data),
+        schema
+      )
+      df.show()
 
-      val sqlDF = spark.sql("SELECT * FROM people")
-      sqlDF.show()
+      val df2 = df.withColumn("test_field", lit("textmext"))
+      df2.show()
+
+      df2.groupBy("word")
+        .agg(sum("number"), count("word"))
+        .show()
+
     }
     catch {
       case e: Exception => println(e.toString)
     }
 
 
-
-
-
-
   }
 
-  def getSparkSession: SparkSession ={
-    return SparkSession
+  def getSparkSession: SparkSession = {
+    SparkSession
       .builder()
       .appName("Spark Example")
       .getOrCreate()
